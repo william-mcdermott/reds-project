9750125142d3a568d67008ad5eeb75514993c8f2
William McDermott <w.e.mcdermott@gmail.com>
2018-12-08 17:52:27 -0500

    add players discussed to dashboard page

--- src/components/TradeListItem.js
+++ src/components/TradeListItem.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
-const TradeListItem = ({ id, comments, team, createdAt }) => (
+const TradeListItem = ({ id, redsPlayers, otherPlayers, comments, team, createdAt }) => (
   // <Link className="list-item" to={`/edit/${id}`}>
   <div className="list-item">
     <div>
       <h3 className="list-item__title">{comments}</h3>
-      <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+      <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY, h:mm a')}</span>
     </div>
     <h3 className="list-item__data">{team}</h3>
+    <p>{`You discussed trading ${redsPlayers} for ${otherPlayers}`}</p>
   </div>
   // </Link>
 )
